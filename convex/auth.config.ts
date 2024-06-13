0bc57248a0b90631efc784ff6eea41accf611709
Jakub Satora <kubasatora@wp.pl>
2024-06-13 17:51:03 +0200

    convex and clerk implemenatation process

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+const authConfig = {
+    providers: [
+        {
+            domain: "https://living-dove-26.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+export default authConfig
---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import {
+    clerkMiddleware,
+    createRouteMatcher
+} from '@clerk/nextjs/server';
+
+const isPublicRoute = createRouteMatcher(["/sing-in", "/sign-up", "/"])
+
+
+
+export default clerkMiddleware((auth, req) => {
+    if (!isPublicRoute(req)) auth().protect()
+});
+
+export const config = {
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
