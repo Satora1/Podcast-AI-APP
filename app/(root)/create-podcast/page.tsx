e39df08afccee0e09a8fad2e953d1a9155f11056
Jakub Satora <kubasatora@wp.pl>
2024-06-19 15:08:40 +0200

    AI talking on page

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -14,7 +14,20 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { cn } from "@/lib/utils"
+import { useState } from "react"
+
+const voiceCategories=['alloy', 'shimmer', 'nova', 'echo', 'fable', 'onyx'];
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -23,6 +36,7 @@ const formSchema = z.object({
 })
 const CreatePodcast = () => {
 
+  const [voiceType, setVoiceType] = useState<string | null>(null)
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
@@ -54,10 +68,36 @@ const CreatePodcast = () => {
                   <FormControl>
                     <Input className="input-class focus-visible:ring-orange-1" placeholder="JSM Pro Podcast" {...field} />
                   </FormControl>
-                  <FormMessage  className="text-white-1"/>
+                  <FormMessage className="text-white-1" />
                 </FormItem>
               )}
             />
+            <div className="flex flex-col gap-2.5">
+              <Label className="text-16 font-bold text-white-1">
+                Select AI voice
+              </Label>
+              <Select onValueChange={(value)=>setVoiceType(value)}>
+                <SelectTrigger className={cn("text-16 w-full border-none bg-black-1 text-gray-1")}>
+                  <SelectValue placeholder="Select AI Voice" className="placeholder:text-gray-1" />
+                </SelectTrigger>
+                <SelectContent className="text-16 border-none bg-black-1 font-bold text-white-1 focus:ring-orange-1">
+                  {voiceCategories.map
+                    ((category) => (
+                      <SelectItem key={category} value={category} className="capitalize focus:bg-orange-1">
+                        {category}
+                      </SelectItem>
+                    ))}
+                </SelectContent>
+                {voiceType && (
+                  <audio
+                  src={`/${voiceType}.mp3`}
+                  autoPlay
+                  className="hidden"
+                  />
+                )}
+              </Select>
+
+            </div>
           </div>
         </form>
       </Form>
